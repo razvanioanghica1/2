17b5ccac6116cbdd1551a5f9481959beba2974a0
razvanioanghica1 <43907291+razvanioanghica1@users.noreply.github.com>
2018-10-06 16:58:17 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,42 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+pip install scrapy
+scrapy startproject olx_houses
+scrapy genspider olx olx.ro
+
+import scrapy
+import datetime
+
+today = datetime.date.today().strftime('%Y-%m-%d')
+
+class OlxHousesSpider(scrapy.Spider):
+    name = 'olx_houses'
+    allowed_domains = ['olx.ro']
+    start_urls = ['https://www.olx.ro/imobiliare/case-de-vanzare/oradea/',
+    'https://www.olx.ro/imobiliare/apartamente-garsoniere-de-inchiriat/oradea/']
+    
+    def parse(self, response):
+        for href in response.css('a.detailsLink::attr(href)'):
+            yield response.follow(href, self.parse_details)
+        for href in  response.css('a.pageNextPrev::attr(href)')[-1:]:
+            yield response.follow(href, self.parse)
+            
+            def parse_details(self, response):
+        attrs = {
+            'url': response.url, 
+            'text': response.css('#textContent>p::text').extract_first().strip(),
+            'title': response.css('h1::text').extract_first().strip(), 
+            'price': response.css('.price-label > strong::text').extract_first().replace(" ", ""),  
+            'date': today, 
+            'nr_anunt':  response.css('.offer-titlebox em small::text').re('\d+'), 
+            'adaugat_la': response.css('.offer-titlebox em::text').re('Adaugat (de pe telefon) +La (.*),') 
+        }
+        
+         for tr in response.css('.details').xpath('tr/td//tr'):
+            title = tr.css('th::text').extract_first()
+            value = " ".join(x.strip() for x in tr.xpath('td/strong//text()').extract() if x.strip()!="")
+            attrs[title]=value
+        yield attrs
+        
+        scrapy run olx -o houses.csv
